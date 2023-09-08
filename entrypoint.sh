d335bdd790e27dcd46f6f58068f46f7ba5d61398
skysyzygy <sky@skysyzygy.xyz>
2023-09-08 15:00:40 -0400

    Add some startup debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+echo "At entrypoint!"
+echo "file_path = $INPUT_FILE_PATH"
+echo "glob = $INPUT_GLOB"
+
 # if the FILE_PATH exists then we will just be uploading the file
 if [[ -z "$INPUT_FILE_PATH" && -z "$INPUT_GLOB" ]]
 then
@@ -9,6 +13,8 @@ then
     echo "Creating archive";
     zip -r "$INPUT_FILE_PATH" ./* -x .git/*
 fi
+
+echo "Running node"
 
 # run the script to send to sharepoint
 node /app/index.js
